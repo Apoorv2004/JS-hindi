071520f72889ed10dbdb316c14e5bd013fe1d67e
Apoorv2004 <itzmenishu04@gmail.com>
2025-06-16 11:16:20 +0530

    learning Objects

--- 08_objects.js
+++ 08_objects.js
@@ -3,10 +3,11 @@
 
 
 // object literals
-
+const mysym = Symbol("my symbol");
 const Jsuper = {
     name: "Apoorv",
     "full name": "Apoorv Chauhan",
+    [mysym] : "This is a symbol property",
     age:20,
     location: "India",
     email:"apoorv@12.com",
@@ -14,7 +15,23 @@ const Jsuper = {
     lastLoginDays: ["Monday", "Tuesday", "Wednesday"]
 }
 
-console.log(Jsuper.email);
-console.log(Jsuper["email"]); // accessing object properties using bracket notation
+// console.log(Jsuper.email);
+// console.log(Jsuper["email"]); // accessing object properties using bracket notation
 
-console.log(Jsuper["full name"]); // accessing object properties with spaces in the name using bracket notation
+// console.log(Jsuper["full name"]); // accessing object properties with spaces in the name using bracket notation
+// console.log(Jsuper[mysym]); // accessing symbol property
+
+// Jsuper.email = "apoorv@chat.com"; // updating object property
+// //Object.freeze(Jsuper); // freezing the object to prevent further modifications
+// Jsuper.email = "ubreed@jc.com"; // this will not change the email property because the object is frozen
+// console.log(Jsuper);
+
+
+Jsuper.greeting = function() { // adding a new method to the object
+    console.log("Hello JS user");
+}
+Jsuper.greetingTwo = function() { // adding a new method to the object
+    console.log(`Hello JS user, ${this.name}`); // using 'this' to access the object property
+}
+console.log(Jsuper.greeting()); // calling the method
+console.log(Jsuper.greetingTwo()); // calling the method with 'this' context
