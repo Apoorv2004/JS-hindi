26f4e6404b686a5034c8ec5ed6fc6e3b995f7e0f
Apoorv2004 <itzmenishu04@gmail.com>
2025-06-28 22:49:16 +0530

    Learing userinput ways

---
+++ 09_Userinput.js
@@ -0,0 +1,9 @@
+// User Inputs
+
+ alert("Hello World!"); 
+
+let myBoolean = confirm("Ok ==== True\nCancel === False");
+console.log(myBoolean); 
+
+let name = prompt("Please enter your name");
+console.log(name ?? "You didnt enter your name.");
